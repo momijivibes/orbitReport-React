76029aae0b94e43048852315be7c7a9c9e376f7f
Amanda Darby <you@example.com>
2024-03-21 16:10:09 -0500

    completed GA 6

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,17 @@
+
+import React from 'react';
+import satData from './satData';
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
+
+<button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
